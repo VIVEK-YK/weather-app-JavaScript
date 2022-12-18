9b7892ba29f23914958e85ff4489247bb638bc1a
VIVEK KUMAR YADAV <88366660+VIVEK-YK@users.noreply.github.com>
2022-12-18 14:25:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log('eh')
+let key = "c05ecbb86b2a46f5ea39d388bdcd41a0";
+
+let search = document.querySelector('#search');
+let button = document.querySelector('#button');
+
+
+
+button.addEventListener('click',()=>{
+    console.log('clicked')
+    var city = '';
+    city = search.value;
+    console.log(city);
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+    
+    fetch(url)
+    .then((response)=>{
+        return response.json();
+    })
+    .then((resp)=>{
+        console.log(resp);
+        let container = document.querySelector('.container');
+        let celcius = Math.round(parseFloat(resp.main.temp)-273.15);
+        console.log(container)
+        console.log(celcius)
+
+
+        container.innerHTML +=`<div class="box">
+        <h2>${city}</h2>
+        <img src="https://openweathermap.org/img/wn/${resp.weather[0].icon}@4x.png" alt="">
+        <p class="deg">${celcius} &deg; c</p>
+        <p>${resp.weather[0].description}</p>
+        <h2>${resp.name}, ${resp.sys.country}</h2>
+      </div>`
+      search.value = '';
+    })
+    .catch((err)=> alert('Pleae Enter Valid City Name'))
+})
+
